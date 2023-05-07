3d55a82a9ff4869a8c968aab34e50d4284934800
benjaminleezhiyuan <113172138+benjaminleezhiyuan@users.noreply.github.com>
2023-05-07 13:42:20 +0800

    Add files via upload

---
+++ tutorial-1/include/glapp.h
@@ -0,0 +1,32 @@
+/* !
+@file    glapp.h
+@author  pghali@digipen.edu
+@date    10/11/2016
+
+This file contains the declaration of namespace GLApp that encapsulates the
+functionality required to implement an OpenGL application including 
+compiling, linking, and validating shader programs
+setting up geometry and index buffers, 
+configuring VAO to present the buffered geometry and index data to
+vertex shaders,
+configuring textures (in later labs),
+configuring cameras (in later labs), 
+and transformations (in later labs).
+*//*__________________________________________________________________________*/
+
+/*                                                                      guard
+----------------------------------------------------------------------------- */
+#ifndef GLAPP_H
+#define GLAPP_H
+
+/*                                                                   includes
+----------------------------------------------------------------------------- */
+
+struct GLApp {
+  static void init();
+  static void update();
+  static void draw();
+  static void cleanup();
+};
+
+#endif /* GLAPP_H */
---
+++ tutorial-1/src/glapp.cpp
@@ -0,0 +1,34 @@
+/*!
+@file    glapp.cpp
+@author  pghali@digipen.edu
+@date    10/11/2016
+
+This file implements functionality useful and necessary to build OpenGL
+applications including use of external APIs such as GLFW to create a
+window and start up an OpenGL context and to extract function pointers
+to OpenGL implementations.
+
+*//*__________________________________________________________________________*/
+
+/*                                                                   includes
+----------------------------------------------------------------------------- */
+#include <glapp.h>
+
+/*                                                   objects with file scope
+----------------------------------------------------------------------------- */
+
+void GLApp::init() {
+  // empty for now
+}
+
+void GLApp::update() {
+  // empty for now
+}
+
+void GLApp::draw() {
+  // empty for now
+}
+
+void GLApp::cleanup() {
+  // empty for now
+}
